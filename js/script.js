6d9b4e3649a0b7cf31a32b5cb4a7264a5764f6a5
DavlatyorBekchanov <klugerklug7@gmail.com>
2024-08-20 06:10:39 +0200

    Notiz qushish

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let notes = ['Banana', 'Rasenmähen'];
+
+
+function renderNotes(){
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = "";
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        const note = notes[indexNote];
+        contentRef.innerHTML += getNoteTemplate(note);
+        
+    }
+}
+
+function getNoteTemplate(note){
+    return ` <p>+ ${note}</p>`;
+}
+
+function addNote(){
+    let noteInputRef = document.getElementById('note_input');
+        let noteInput = noteInputRef.value;
+        notes.push(noteInput);
+        renderNotes()
+        noteInputRef.value = "";
+}
