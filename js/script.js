ca27747b23fcdb8993d59a29c2a200252deae8ca
DavlatyorBekchanov <klugerklug7@gmail.com>
2024-08-20 07:36:19 +0200

    Trash qushilgani

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
 let notes = ['Banana', 'Rasenmähen'];
 
+let trashNotes = [];
 
 function renderNotes(){
     let contentRef = document.getElementById('content');
     contentRef.innerHTML = "";
     for (let indexNote = 0; indexNote < notes.length; indexNote++) {
        
-        contentRef.innerHTML += getNoteTemplate(indexNote);
+        contentRef.innerHTML += getTrashNoteTemplate(indexNote);
+        
+    }
+}
+function renderTrashnotes(){
+    let trashContentRef = document.getElementById('trash_content');
+    trashContentRef.innerHTML = "";
+    for (let indexTrashNote = 0; trashNotes < trashNotes.length; indexTrashNote++) {
+       trashContentRef.innerHTML += getNoteTemplate(indexTrashNote) 
         
     }
 }
 
+
+
+
 function getNoteTemplate(indexNote){
     return ` <p>+ ${notes[indexNote]} <button onclick = "deletNote(${indexNote})">X</button></p> `;
 }
+
+function getTrashNoteTemplate(indexTrashNote){
+    return ` <p>+ ${trashNotes[indexTrashNote]} <button onclick = "deletNote(${indexTrashNote})">X</button></p> `;
+}
+
 
 function addNote(){
     let noteInputRef = document.getElementById('note_input');
@@ -24,6 +41,9 @@ function addNote(){
 }
 
 function deletNote(indexNote){
-    notes.splice(indexNote, 1)
+    let trashNote = notes.splice(indexNote, 1)
+    trashNotes.push(trashNote)
     renderNotes();
+    renderTrashnotes()
+
 }
